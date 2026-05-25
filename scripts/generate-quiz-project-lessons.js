0255e9caf4e92a916ddf6ddfb202718fb91c3e44
Artem Hrechuk <hrechuk.artem@gmail.com>
2026-05-25 02:07:36 +0200

    feat: create script structure for quiz/project lesson generation

---
+++ scripts/generate-quiz-project-lessons.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env bun
+/**
+ * Script to generate quiz and project lessons for all units in all tracks.
+ * Reads tracks.json and units.json to determine the structure.
+ * Creates lesson files in site/src/content/lessons/{en,ru}/{track}/{unit}/{lesson}/index.mdx
+ */
