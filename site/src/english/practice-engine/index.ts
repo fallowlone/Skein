cd4f5de76c0f920b9996863636995a1455a55126
Artem Hrechuk <hrechuk.artem@gmail.com>
2026-06-15 18:16:31 +0200

    feat(english): practice-engine generate() orchestrator + barrel

---
+++ site/src/english/practice-engine/index.ts
@@ -0,0 +1,4 @@
+export { generate, generateFromSpec, type GenerateOpts } from "./generate";
+export { composite } from "./cross-topic";
+export { validateExercise } from "./validate";
+export type { GeneratedExercise } from "./types";
